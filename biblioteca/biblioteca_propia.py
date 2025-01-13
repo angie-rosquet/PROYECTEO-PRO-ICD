71617e01851aab65670ae11b7e8c7383b0ccc832
angie rosquet <angierosquet@gmail.com>
2025-01-12 21:00:02 -0500

    6

---
+++ biblioteca/biblioteca_propia.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import json
+import os
+import glob
+
+#La ruta de la carpeta donde estan los JSON
+ruta = 'Lugares/'
+
+#funcion que permite almacenar los datos en una lista
+def datos_restaurantes(ruta):
+    restaurantes = []
+    # un bucle que va a ir generando la ruta de cada una de las carpetas dentro de Lugares, las cuales son los municipios
+    for lugar in os.listdir(ruta):
+        #crear la variable que crea la ruta del lugar
+        ubicacion_municipio = os.path.join(ruta,lugar)
+        #verificar que los elementos de la carpeta Lugares son carpetas con isdir
+        if os.path.isdir(ubicacion_municipio):
+            #si es una carpeta entonces procede
+            #ahora tengo que recorrer cada archivo dentro de cada ubicacion_municipio
+            for archivo in os.listdir(ubicacion_municipio):
+                #Tengo que abrir en modo lectura cada archivo que este dado por la ruta que se designa como ruta_archivo
+                with open(os.path.join(ubicacion_municipio, archivo), 'r', encoding='utf-8') as ruta_archivo:
+                    #agregar cada restaurante a la lista de restaurantes
+                    restaurantes.append(json.load(ruta_archivo))
+    return restaurantes
+
+#funcion que cree el DataFrame de la lista
+def df_datos(array):
+    array_df = pd.DataFrame(array)
+    return array_df
+
+
+datos = datos_restaurantes(ruta)
+df_datos = df_datos(datos)
+
+print(len(df_datos))
