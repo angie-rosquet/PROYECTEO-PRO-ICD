6c351c8a8c7c25b79b631f1fc113d0ed7f0e73eb
angie rosquet <angierosquet@gmail.com>
2025-01-12 20:58:04 -0500

    5

--- biblioteca_propia.py
+++ biblioteca_propia.py
@@ -2,23 +2,22 @@ import pandas as pd
 import json
 import os
 import glob
-
+#La ruta de la carpeta donde estan mis JSON
 ruta = 'Lugares/'
-
+#Lista vacia donde se va a ir almacenando la info de cada restaurante
 restaurantes_datos = []
-
+#Crear una lista que contenga el nombre de cada una de las carpetas dentro de lugares, que se corresponde con los 15 Municipios
 municipios = os.listdir(ruta)
 for municipio in municipios :
     ruta_municipio = os.path.join(ruta, municipio)
-if os.path.isdir(ruta_municipio):
-    archivos_json = glob.glob(os.path.join(ruta_municipio, '*.json'))
-    for archivo in archivos_json:
-        with open(archivo, 'r', encoding='utf-8') as f:
-            datos_json = json.load(f)
-            datos_json['municipio'] = municipio
-            restaurantes_datos.append(datos_json)
+    if os.path.isdir(ruta_municipio):
+        archivos_json = glob.glob(os.path.join(ruta_municipio, '*.json'))
+        for archivo in archivos_json:
+            with open(archivo, 'r', encoding='utf-8') as f:
+                datos_json = json.load(f)
+                datos_json['municipio'] = municipio
+                restaurantes_datos.append(datos_json)
 
 
 df_restaurantes = pd.DataFrame(restaurantes_datos)
-pd.set_option('display.max_rows', None)
 print(df_restaurantes)
